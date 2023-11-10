f2b7e5d43be85a796de6aa310eeeffc9604d63dd
DinowRowr <150400150+DinowRowr@users.noreply.github.com>
2023-11-10 08:49:43 +0800

    Add files via upload

---
+++ Python/Basics.py
@@ -0,0 +1,17 @@
+print('Hello World!')
+print('o----')
+print(' ||||')
+print("=" * 100)
+
+patient = 'John Smith'
+age = 20
+newPatient = True
+
+name = input('What is your name? ')
+print('Hello ' + name)
+color = input('What is your favorite color? ')
+print('Wow! A color ' + color +', I like that color too!')
+
+birthYear = input('What is your birthyear? ')
+age = 2021 - int(birthYear)
+print(age)
